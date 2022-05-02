a62346b51d053fa3e259416af0f1f25f38c33b35
btokmach <btokmach@gmail.com>
2022-05-02 14:48:24 +0300

    Basic SEO optimization has been implemented with react-helmet

--- src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet";
+
 import './singleCharacterLayout.scss';
 
 const SingleCharacterLayout = ({data}) => {
@@ -6,6 +8,13 @@ const SingleCharacterLayout = ({data}) => {
 
     return (
         <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${name} page`}
+                />
+                <title>{name}</title>
+            </Helmet>
             <img src={thumbnail} alt={name} className="single-comic__char-img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{name}</h2>
